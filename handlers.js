e8e2b962861226e261e632c8f14db83a9bd4c192
Иван <kleimenov416@gmail.com>
2020-10-28 13:30:47 -0400

    made source code in handlers.js file clean

--- handlers.js
+++ handlers.js
@@ -2,11 +2,11 @@
 
 //1. randomIdcreator function will create random user id. Function will create user id in range from 1 to 1000
 const randomUserIdGen = () => {
-    const minNumber = 11;
-    const maxNumber = 1000;
-    let randomId = Math.random() * (maxNumber - minNumber) + minNumber;
-    return Math.round(randomId)
-    };
+  const minNumber = 11;
+  const maxNumber = 1000;
+  let randomId = Math.random() * (maxNumber - minNumber) + minNumber;
+  return Math.round(randomId);
+};
 
 
 
