3cd1914956d11712ffba86d56204a44be8daae6c
Иван <kleimenov416@gmail.com>
2020-11-10 17:41:20 -0500

    start working at frontend part of project

--- public/static/index.js
+++ public/static/index.js
@@ -1,4 +1,4 @@
-
+/*
 const createIngredientField = () => {
     console.log('hey')
     let ingredientFieldContainer = document.createElement("div");
@@ -15,5 +15,39 @@ const createIngredientField = () => {
 const addIngredient = document.getElementById('addIngredient');
 
 addIngredient.addEventListener('click', createIngredientField());
+*/
+/*
+<script>
+        const createIngredientField = () => {
+            let ingredientFieldContainer = document.createElement("div");
+            let ingredientNameField = document.createElement("input");
+            ingredientNameField.name = `ingredient`;
+            ingredientNameField.placeholder = `Amount`;
+            let ingredientAmountField = document.createElement("input");
+            ingredientAmountField.name = `amount`;
+            ingredientAmountField.placeholder = `Amount`;
+            ingredientFieldContainer.appendChild(ingredientNameField);
+            ingredientFieldContainer.appendChild(ingredientAmountField);
+            const ingredientsBox = document.getElementById('ingredients-field');
+            ingredientsBox.appendChild(ingredientFieldContainer);
+        }
+        const addIngredient = document.getElementById('addIngredient');
+        addIngredient.addEventListener('click', createIngredientField);
+    </script>
+*/
 
-
+const createIngredientField = () => {
+    let ingredientFieldContainer = document.createElement("div");
+    let ingredientNameField = document.createElement("input");
+    ingredientNameField.name = `ingredient`;
+    ingredientNameField.placeholder = `Amount`;
+    let ingredientAmountField = document.createElement("input");
+    ingredientAmountField.name = `amount`;
+    ingredientAmountField.placeholder = `Amount`;
+    ingredientFieldContainer.appendChild(ingredientNameField);
+    ingredientFieldContainer.appendChild(ingredientAmountField);
+    const ingredientsBox = document.getElementById('ingredients-field');
+    ingredientsBox.appendChild(ingredientFieldContainer);
+}
+const addIngredient = document.getElementById('addIngredient');
+addIngredient.addEventListener('click', createIngredientField);
