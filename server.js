3a09d2ac1022f4af53758e37a11879d2bf4da7e6
Иван <kleimenov416@gmail.com>
2021-01-07 09:23:32 -0500

    marked absolute path of the directory

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cookieParser());
 
+//absolute path of the directory
 app.use(express.static(path.join(__dirname, 'public')))
 
 //----------+----------------+----------+----------------+----------+----------------+----------+----------------
