d69d4cb6ae9f04e62c4f5d395acb2274359f68cf
Иван <kleimenov416@gmail.com>
2021-01-08 11:52:04 -0500

    add new query getReviewByCocktailIdUserId, this query afford get full information about review

--- public/static/addNewReview.js
+++ public/static/addNewReview.js
@@ -2,17 +2,11 @@ const addReviewButton = document.getElementById('addReview');
 const reviewHolder = document.getElementById('reviewHolder');
 const submitReview = document.getElementById('submitReview');
 
-const submitRev = (submit) => {
-    submit.removeEventListener('click', (evt) => {
-    evt.preventDefault();
-    return console.log('hello')
-})
 
-}
+
 
 addReviewButton.addEventListener('click', (evt) => {
     evt.preventDefault();
     reviewHolder.classList.remove('visually-hidden');
-    submitRev(submitReview)
 })
 
