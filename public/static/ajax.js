ff31e6b593f92b278e4704a1e1533671ccc46bd3
Иван <kleimenov416@gmail.com>
2021-01-11 10:36:17 -0500

    start explore AJAX inside ajax.js file

---
+++ public/static/ajax.js
@@ -0,0 +1,8 @@
+//Let's try to learn and even implement AJAX (Asynchronous Javascript and XML) into my app
+
+/* 
+'Cocktail likes/dislikes counter' will be first feature where I impliment AJAX 
+Algorithm:
+- add likes counter (with JS in scrath)
+- add AJAX on counter (it is afford add likes/dislikes and doesn't reload full page)
+*/
